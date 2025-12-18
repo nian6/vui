6eccb90eaf53065093ea1650fb80f36d6b51e97e
nian <2459716150@qq.com>
2025-12-18 18:18:03 +0800

    add: add @element-plus/icons-vue dep

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'vue';
 import DefaultTheme from 'vitepress/theme';
 import { ElementPlusContainer } from '@vitepress-demo-preview/component';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 import '@vitepress-demo-preview/component/dist/style.css';
 
 import vui from 'vui';
@@ -10,6 +11,9 @@ export default {
   ...DefaultTheme,
   enhanceApp({ app }: { app: App }) {
     app.component('DemoPreview', ElementPlusContainer);
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component);
+    }
     app.use(vui);
   },
 };
